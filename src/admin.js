127911500d8765bd3601cd5194a1ac109fb3802e
markustenghamn <markus@tenghamn.com>
2020-08-07 23:53:26 +0200

    Adds ability to tag pages Part of markustenghamn/beubo#20

--- src/admin.js
+++ src/admin.js
@@ -3,9 +3,15 @@ import jquery from 'jquery'
 import trumbowyg from 'trumbowyg'
 
 import '../node_modules/trumbowyg/dist/ui/trumbowyg.css'
+import '../node_modules/@yaireo/tagify/dist/tagify.css'
 import './admin.css'
 
 // Any content field should use WYSIWYG editor
 $ ('#contentField').trumbowyg ({
     svgPath: '/default/js/ui/icons.svg'
 });
+
+let input = document.querySelector ('input[name=tagField]');
+if (input !== null) {
+    let tagify = new Tagify (input)
+}
