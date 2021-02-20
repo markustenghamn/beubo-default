efbd621b4c519131eea932c9634e25639b4d9464
uberswe <markus@tenghamn.com>
2021-02-20 16:56:32 +0100

    Removed non-needed packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 const path = require ('path');
-const webpack = require ('webpack');
 const MiniCssExtractPlugin = require ('mini-css-extract-plugin');
-const CopyPlugin = require ('copy-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+
 
 module.exports = {
     mode: 'production',
@@ -17,30 +16,25 @@ module.exports = {
         path: path.resolve (__dirname, 'js'),
     },
     plugins: [
-        new webpack.ProvidePlugin ({
-            jQuery: 'jquery',
-            $: 'jquery'
-        }),
         new MiniCssExtractPlugin ({
             // Options similar to the same options in webpackOptions.output
             // both options are optional
             filename: '../css/[name].css',
             chunkFilename: '../css/[id].css',
         }),
-        new CopyPlugin ({
-            patterns: [
-                {from: './node_modules/trumbowyg/dist/ui/icons.svg', to: './ui/icons.svg'},
-            ],
-            options: {
-                concurrency: 100,
-            },
-        }),
     ],
     module: {
         rules: [
             {
-                test: /\.svg$/,
-                loader: 'svg-inline-loader'
+                test: /\.(svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '../images/[name].[ext]',
+                        },
+                    },
+                ],
             },
             {
                 test: /\.css$/i,
